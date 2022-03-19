9ddbde5566843a1fcc3beb2ebe990280488ad436
Zach Cook <zach@web3technologies.io>
2022-03-19 13:17:57 -0500

    added logging

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,26 @@ import MainContainer  from '../containers/main'
 import TeamContainer from '../containers/team'
 import RoadMapContainer from '../containers/roadmap'
 import FAQSContainer from '../containers/faqs'
-
+import { useEffect } from 'react'
 
 export default function Home() {
+
+
+  useEffect(()=>{
+
+    async function logHome(){
+
+      try{
+        const res = await fetch("http://localhost:8000/usage/homepage/")
+      }catch(e){
+        console.log(e)
+      }
+      
+    }
+
+    logHome()
+
+  }, [])
 
   return (
       <>
