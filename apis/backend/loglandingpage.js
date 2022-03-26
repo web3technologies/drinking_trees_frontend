67879959b9d5620c8e215ac0ae3ed696cec63325
Zach Cook <zach@web3technologies.io>
2022-03-26 16:12:46 -0500

    updates to the page

---
+++ apis/backend/loglandingpage.js
@@ -0,0 +1,14 @@
+import {baseBackendUrl} from '../../config/config';
+import axios from 'axios'
+
+
+export default async function logLandingPage(){
+
+    try{
+        const res = await axios.get(`${baseBackendUrl}/logging/landingpage`)
+    } catch (e){
+        console.log(e)
+    }
+    
+
+}
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,12 @@ import MainContainer  from '../containers/main'
 import TeamContainer from '../containers/team'
 import RoadMapContainer from '../containers/roadmap'
 import FAQSContainer from '../containers/faqs'
+import logLandingPage from '../apis/backend/loglandingpage'
+
 
 export default function Home() {
+
+  logLandingPage()
 
   return (
       <>
