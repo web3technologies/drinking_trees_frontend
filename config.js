501c87500b70144f67680699f2f895c60b29b95f
Zach <zach@web3technologies.io>
2022-03-29 21:03:26 -0500

    updated to fetch abi data from s3

--- config.js
+++ config.js
@@ -9,3 +9,8 @@ export const bankAddress = drinkingTreesBankData.address
 export const baseUrl = "https://gateway.pinata.cloud/ipfs/"
 export const baseUri = "QmRE1aNGV8SEt5jcbKya6awzKuvSAdfRP73MWcgrF8wbML/"
 
+
+const contractEnvironment = process.env.NEXT_PUBLIC_CONRACT_ENVIRONMENT
+
+export const s3ContractLinkBase = `https://drinkingtrees.s3.us-east-2.amazonaws.com/${contractEnvironment}`
+
