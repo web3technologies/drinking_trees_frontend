fdb851c819759370b2c8de84e9bda9ada0a3455c
Zach Cook <zach@web3technologies.io>
2022-04-03 20:42:12 -0500

    updated the timer

--- containers/time.js
+++ containers/time.js
@@ -4,7 +4,7 @@ import { Main }   from '../styles/components';
 const TimeFrame = ()=>{
     // used to prevent reload of the entire main container each second
     const { time } = useTimer()
-    return <Main.CountDownText>{time}</Main.CountDownText>
+    return <Main.CountDownText>{time}&#x200b;</Main.CountDownText>
 }
 
 
