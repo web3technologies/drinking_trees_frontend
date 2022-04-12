7d0435b860089abc1ef6b611712f61dcf905d160
pswnr9 <pswnr9@gmail.com>
2022-04-11 21:28:02 -0500

    details page - error clean

--- containers/details.js
+++ containers/details.js
@@ -1,13 +1,12 @@
 import React from "react";
-import { useEffect } from "react";
 import useMyTrees from "../hooks/usemytrees";
 import { ImageDetailsPage } from "../styles/components/imageGallery/details";
-// import MockData from "../mockData/test.json";
+
 
 export default function DetailsContainer() {
   const { userAssets, handlePriceChange, loading } = useMyTrees();
 
- ?
+ 
 
   return (
     <>
