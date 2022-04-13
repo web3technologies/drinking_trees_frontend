ccd520df506570adf4d4effc93a5b11ee191a415
Zach Cook <zach@web3technologies.io>
2022-04-13 07:00:28 -0500

    added video and updated number

--- containers/main.js
+++ containers/main.js
@@ -18,6 +18,14 @@ const pStyle = {
     color: "#4BF493"
 }
 
+const videoStyle = {
+        position: "relative",
+        overflow: "hidden",
+        textAlign: "center",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+}
 
 export default function MainContainer(){
 
@@ -25,10 +33,15 @@ export default function MainContainer(){
     return (
         <Main>
             <Main.MainFrameInner>
+{/* 
+                <video  autoplay="autoplay" controls="controls">
+                    <source src="https://drinkingtrees.s3.us-east-2.amazonaws.com/examples/Drinking+Trees+Website.mp4" type="video/mp4"/>
+                </video> */}
+
                 <Main.MainFrameLeft>
                     <Main.MainText>Drinking Trees NFT <a style={{color: "#049829"}}>Collection</a></Main.MainText>
                     <Main.SupportTextFrame>
-                        <Main.SupportingText>In celebration of Arbor Day, we bring you 3,333 Drinking Trees</Main.SupportingText>
+                        <Main.SupportingText>In celebration of Arbor Day, we bring you 5,000 Drinking Trees</Main.SupportingText>
                         <Main.SupportingText>10% of all proceeds donated to <a style={pStyle} onClick={()=>safeOpen("https://onetreeplanted.org/")}>One Tree Planted</a></Main.SupportingText>
                         <Main.SupportingText>Mint Period: Starts the day before Arbor Day 4/28 - 5/1 at 1:00pm ET</Main.SupportingText>
                         <Main.MultivacFrame onClick={addMultiVac}>
