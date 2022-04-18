4b15fc850938e92488e0c74c889a952336c53bf1
Zach <zach@web3technologies.io>
2022-04-18 10:58:38 -0500

    more updates

--- pages/_app.js
+++ pages/_app.js
@@ -11,19 +11,20 @@ function MyApp({ Component, pageProps }) {
 
   const { user, chain, hasMetaMask, loadingUser, loadUser, switchNetwork} = useConnection()
 
-  // if(process.env.NEXT_PUBLIC_ENVIRONMENT === "staging"){
-  //   if (!user.isAdminUser){
-  //     return (
-  //       <AdminContainer
-  //         chain={chain} 
-  //         loadUser={loadUser}
-  //         switchNetwork={switchNetwork}
-  //         loadingUser={loadingUser}
-  //     />
-  //     )
+  if(process.env.NEXT_PUBLIC_ENVIRONMENT === "staging"){
+    // if (!user.isAdminUser){
+    if(false){
+      return (
+        <AdminContainer
+          chain={chain} 
+          loadUser={loadUser}
+          switchNetwork={switchNetwork}
+          loadingUser={loadingUser}
+      />
+      )
       
-  //   }
-  // }
+    }
+  }
   
   return (
     <>
