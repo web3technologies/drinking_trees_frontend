b529a16be8b884686d5265212ed825d097242ad1
pswnr9 <pswnr9@gmail.com>
2022-05-24 19:42:43 -0500

    build failed

--- styles/components/main/index.js
+++ styles/components/main/index.js
@@ -4,7 +4,7 @@ import {
      MainFrameRight, MainText, SupportTextFrame, SupportingText, ImageFrame, BlueDiv, PurpleDiv,
      Image, CountDownFrame, CountDownText, 
      ExploreDiv, CollectionLink, LinkLogo, LoadingFrame, LogoFrame,
-    Landing, MovingTrees, LogoDiv, SiteURL, Navbar, Listing, ListingSocial, ContentDiv, ContentInner, Heading, ButtonExplore, Discord, Twitter, Metamask} from './styles/main'
+     Heading} from './styles/main'
 
 export default function Main({children, ...restProps}) {
     return <MainFrame {...restProps}>{children}</MainFrame>
@@ -46,12 +46,7 @@ Main.LinkLogo = function MainLinkLogo({children, ...restProps}) {
 Main.MainFrameRight = function MainMainFrameRight({children, ...restProps}) {
     return <MainFrameRight {...restProps}> {children}</MainFrameRight>
 }
-Main.ContentDiv = function MainContentDiv({children, ...restProps}) {
-    return <ContentDiv {...restProps}> {children}</ContentDiv>
-}
-Main.ContentInner = function MainContentInner({children, ...restProps}) {
-    return <ContentInner {...restProps}> {children}</ContentInner>
-}
+
 Main.ImageFrame = function MainImageFrame({children, ...restProps}) {
     return <ImageFrame {...restProps}> {children}</ImageFrame>
 }
@@ -59,36 +54,14 @@ Main.BlueDiv = function MainBlueDiv({children, ...restProps}) {
     return <BlueDiv {...restProps}> {children}</BlueDiv>
 }
 
-Main.LogoDiv = function MainLogoDiv({children, ...restProps}) {
-    return <LogoDiv {...restProps}> {children}</LogoDiv>
-}
-Main.SiteURL = function MainSiteURL({children, ...restProps}) {
-    return <SiteURL {...restProps}> {children}</SiteURL>
-}
-Main.Listing = function MainListing({children, ...restProps}) {
-    return <Listing {...restProps}> {children}</Listing>
-}
-Main.ListingSocial = function MainListingSocial({children, ...restProps}) {
-    return <ListingSocial {...restProps}> {children}</ListingSocial>
-}
-Main.Navbar = function MainNavbar({children, ...restProps}) {
-    return <Navbar {...restProps}> {children}</Navbar>
-}
+
+
+
 Main.Heading = function MainHeading({children, ...restProps}) {
     return <Heading {...restProps}> {children}</Heading>
 }
-Main.ButtonExplore = function MainButtonExplore({children, ...restProps}) {
-    return <ButtonExplore {...restProps}> {children}</ButtonExplore>
-}
-Main.Discord = function MainDiscord({children, ...restProps}) {
-    return <Discord {...restProps}> {children}</Discord>
-}
-Main.Twitter = function MainTwitter({children, ...restProps}) {
-    return <Twitter {...restProps}> {children}</Twitter>
-}
-Main.Metamask = function MainMetamask({children, ...restProps}) {
-    return <Metamask {...restProps}> {children}</Metamask>
-}
+
+
 Main.PurpleDiv = function MainPurpleDiv({children, ...restProps}) {
     return <PurpleDiv {...restProps}> {children}</PurpleDiv>
 }
@@ -96,9 +69,7 @@ Main.PurpleDiv = function MainPurpleDiv({children, ...restProps}) {
 Main.Image = function MainImage({children, ...restProps}) {
     return <Image {...restProps}></Image>
 }
-Main.Landing = function MainLanding({children, ...restProps}) {
-    return <Landing {...restProps}></Landing>
-}
+
 Main.CountDownFrame = function MainCountDownFrame({children, ...restProps}) {
     return <CountDownFrame {...restProps}> {children}</CountDownFrame>
 }
